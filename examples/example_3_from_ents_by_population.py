8551c0ecba8a2f699df6db4f2576eea2f323f9d5
nuuuwan <nuuuwan@gmail.com>
2024-06-10 20:40:05 +0530

    various

---
+++ examples/example_3_from_ents_by_population.py
@@ -0,0 +1,24 @@
+def main():
+    import os
+
+    from gig import Ent, EntType
+
+    from cac import DNC
+
+    ents = Ent.list_from_type(EntType.DISTRICT)
+    total_population = sum([ent.population for ent in ents])
+    id_to_value = {}
+    for ent in ents:
+        id_to_value[ent.id] = ent.population / total_population
+
+    dnc = DNC.from_ents(ents, id_to_value)
+    dir_output = os.path.join(
+        'example_images',
+        os.path.basename(__file__)[:-3],
+    )
+    dnc.run(dir_output)
+
+
+
+if __name__ == "__main__":
+    main()
