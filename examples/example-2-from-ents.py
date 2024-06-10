6f80401eb81aacd2dd4a9066717a26a920c6b5fa
nuuuwan <nuuuwan@gmail.com>
2024-06-10 18:12:12 +0530

    uncomment

--- examples/example-2-from-ents.py
+++ examples/example-2-from-ents.py
@@ -37,8 +37,8 @@ def custom_western():
 
 
 if __name__ == "__main__":
-    # from_ent_type(EntType.PROVINCE)
-    # from_ent_type(EntType.DISTRICT)
-    # from_ent_type(EntType.PD)
-    # custom_colombo()
+    from_ent_type(EntType.PROVINCE)
+    from_ent_type(EntType.DISTRICT)
+    from_ent_type(EntType.PD)
+    custom_colombo()
     custom_western()
