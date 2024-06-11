9ccd32ad600efa0c309a470feefdbfcd750c21fe
nuuuwan <nuuuwan@gmail.com>
2024-06-11 08:30:09 +0530

    SIMPLIFIED save_image

--- src/cac/dnc/DNCRenderer.py
+++ src/cac/dnc/DNCRenderer.py
@@ -23,40 +23,61 @@ class DNCRenderer:
         return f'#{r:02x}{g:02x}{b:02x}'
 
     @staticmethod
-    def save_image(grouped_polygons, image_path):
-        plt.close()
-        ax = plt.gca()
-        for i, grouped_polygon in enumerate(grouped_polygons):
-            shapely_polygon = grouped_polygon.shapely_polygon
-            log2_error = grouped_polygon.log2_error
-            gdf = topojson.Topology(shapely_polygon).to_gdf()
-            background_color = DNCRenderer.get_color(log2_error)
-            gdf.plot(
-                ax=ax,
-                facecolor=background_color,
-                edgecolor="white",
-                linewidth=0.1,
-            )
+    def render_polygon_shape(grouped_polygon, ax):
+        shapely_polygon = grouped_polygon.shapely_polygon
+        log2_error = grouped_polygon.log2_error
+        gdf = topojson.Topology(shapely_polygon).to_gdf()
+        background_color = DNCRenderer.get_color(log2_error)
+        gdf.plot(
+            ax=ax,
+            facecolor=background_color,
+            edgecolor="white",
+            linewidth=0.1,
+        )
 
-            foreground_color = DNCRenderer.get_foreground_color(
-                background_color
-            )
-            x, y = shapely_polygon.centroid.coords[0]
-            plt.text(
-                x,
-                y,
-                f'{log2_error:.2f}',
-                color=foreground_color,
-                fontsize=3,
-                horizontalalignment='center',
-                verticalalignment='center',
-            )
+    @staticmethod
+    def render_polygon_text(grouped_polygon):
+        log2_error = grouped_polygon.log2_error
+        shapely_polygon = grouped_polygon.shapely_polygon
+        background_color = DNCRenderer.get_color(log2_error)
 
+        foreground_color = DNCRenderer.get_foreground_color(background_color)
+        x, y = shapely_polygon.centroid.coords[0]
+        plt.text(
+            x,
+            y,
+            f'{log2_error:.2f}',
+            color=foreground_color,
+            fontsize=3,
+            horizontalalignment='center',
+            verticalalignment='center',
+        )
+
+    @staticmethod
+    def render_polygon(grouped_polygon, ax):
+        DNCRenderer.render_polygon_shape(grouped_polygon, ax)
+        DNCRenderer.render_polygon_text(grouped_polygon)
+
+    @staticmethod
+    def remove_grids(ax):
         ax.set_xticks([])
         ax.set_yticks([])
         ax.grid(False)
         for spine in ax.spines.values():
             spine.set_visible(False)
 
+    @staticmethod
+    def render_all(
+        grouped_polygons,
+    ):
+        plt.close()
+        ax = plt.gca()
+        for grouped_polygon in grouped_polygons:
+            DNCRenderer.render_polygon(grouped_polygon, ax)
+        DNCRenderer.remove_grids(ax)
+
+    @staticmethod
+    def save_image(grouped_polygons, image_path):
+        DNCRenderer.render_all(grouped_polygons)
         plt.savefig(image_path, dpi=300, bbox_inches='tight', pad_inches=0)
         log.info(f'Wrote {image_path}')
