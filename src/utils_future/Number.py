ad9d204065c2d01e70cebd08e3eb353cce68adda
nuuuwan <nuuuwan@gmail.com>
2024-06-11 15:32:50 +0530

    minor

--- src/utils_future/Number.py
+++ src/utils_future/Number.py
@@ -15,6 +15,4 @@ class Number:
             return f'{x/1_000_000.0:,.2f}M'
         if x > 1_000:
             return f'{x/1_000.0:,.0f}K'
-        if x > 1:
-            return f'{x:,.0f}'
-        return f'{x:.3f}'
+        return f'{x:.3g}'
