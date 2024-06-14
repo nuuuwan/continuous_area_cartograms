02cfbde7e9e80f0235327522017146c3d933f0e8
nuuuwan <nuuuwan@gmail.com>
2024-06-14 22:04:13 +0530

    various

---
+++ examples/lk_province_by_religion/source.py
@@ -0,0 +1,28 @@
+def main():
+    import os
+
+    from gig import Ent, EntType, GIGTable
+
+    from cac import DNC
+
+    gig_table_last_election = GIGTable(
+        'population-religion', 'regions', '2012'
+    )
+   
+    ents = [ent for ent in Ent.list_from_type(EntType.PROVINCE)]
+    values = []
+    for ent in ents:
+        row = ent.gig(gig_table_last_election)
+        values.append(row.islam)
+    
+    dnc = DNC.from_ents(ents, values)
+    dnc.run(
+        os.path.join(
+            os.path.dirname(__file__),
+            'output',
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
