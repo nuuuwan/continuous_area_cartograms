6ecce707d8e8622a01d33e1730f9ee44626562cb
nuuuwan <nuuuwan@gmail.com>
2024-06-14 11:37:45 +0530

    Added europe_by_gdp_md_est

---
+++ examples/europe_by_gdp_md_est/source.py
@@ -0,0 +1,29 @@
+def main():
+    import os
+
+    import geopandas
+
+    from cac import DNC
+
+    gdf = geopandas.read_file(
+        geopandas.datasets.get_path('naturalearth_lowres')
+    )
+    gdf = gdf[gdf['continent'] == 'Europe']
+    gdf = gdf[gdf['name'] != 'Russia']
+
+    values = gdf['gdp_md_est'].tolist()
+    dnc = DNC.from_gdf(
+        gdf,
+        values,
+
+    )
+    dnc.run(
+        os.path.join(
+            os.path.dirname(__file__),
+            'output',
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
