ae11efa634a31de3fd4b06eea17d64e73e5ac979
nuuuwan <nuuuwan@gmail.com>
2024-07-11 14:28:52 +0530

    lint

--- src/cac/algos/dcn/impl/DCN1985Runner.py
+++ src/cac/algos/dcn/impl/DCN1985Runner.py
@@ -1,7 +1,6 @@
 import os
 import shutil
 import tempfile
-import time
 
 import numpy as np
 from shapely import Polygon, affinity
@@ -13,6 +12,32 @@ log = Log('DCN1985Runner')
 
 
 class DCN1985Runner:
+    @staticmethod
+    def get_Delta_i(Point_i, Centroid):
+        return np.array(
+            Point_i[np.newaxis, :, :] - Centroid[:, np.newaxis, :],
+            dtype=np.float64,
+        )
+
+    @staticmethod
+    def get_Point_i_incr(
+        Distance_i, Radius, Mass, Angle_i, force_reduction_factor
+    ):
+        return (
+            np.sum(
+                np.where(
+                    Distance_i > Radius,
+                    Mass * (Radius / Distance_i),
+                    Mass
+                    * (Distance_i**2 / Radius**2)
+                    * (4 - 3 * (Distance_i / Radius)),
+                )
+                * np.array([np.cos(Angle_i), np.sin(Angle_i)]),
+                axis=2,
+            ).transpose()
+            * force_reduction_factor
+        )
+
     @classmethod
     def run_single_optimized(cls, dcn):
         force_reduction_factor = dcn.force_reduction_factor
@@ -23,28 +48,13 @@ class DCN1985Runner:
 
         new_Point = []
         for Point_i in Point:
-            Delta_i = np.array(
-                Point_i[np.newaxis, :, :] - Centroid[:, np.newaxis, :],
-                dtype=np.float64,
-            )
+            Delta_i = DCN1985Runner.get_Delta_i(Point_i, Centroid)
             Distance_i = np.linalg.norm(Delta_i, axis=2).transpose()
             Angle_i = np.arctan2(
                 Delta_i[:, :, 1], Delta_i[:, :, 0]
             ).transpose()
-
-            Point_i += (
-                np.sum(
-                    np.where(
-                        Distance_i > Radius,
-                        Mass * (Radius / Distance_i),
-                        Mass
-                        * (Distance_i**2 / Radius**2)
-                        * (4 - 3 * (Distance_i / Radius)),
-                    )
-                    * np.array([np.cos(Angle_i), np.sin(Angle_i)]),
-                    axis=2,
-                ).transpose()
-                * force_reduction_factor
+            Point_i += DCN1985Runner.get_Point_i_incr(
+                Distance_i, Radius, Mass, Angle_i, force_reduction_factor
             )
             new_Point.append(Point_i)
 
@@ -65,26 +75,27 @@ class DCN1985Runner:
             new_polygons.append(polygon)
         return dcn.from_dcn(new_polygons)
 
+    @staticmethod
+    def save_partial(i_iter, dcn, dir_image, dir_output):
+        # save image
+        file_id = f'{i_iter:03}'
+        image_path = os.path.join(dir_image, f'{file_id}.png')
+        dcn.save_image(image_path, i_iter)
+
+        # save gdf
+        gdf_path = os.path.join(dir_output, 'geojson', f'{file_id}.json')
+        dcn.to_gdf().to_file(gdf_path, driver='GeoJSON')
+
     @classmethod
     def run_all(cls, dcn0, dir_output):
         dcn = dcn0
         dcn.log_complexity()
 
         i_iter = 0
-        t_start = time.time()
         dir_image = os.path.join(dir_output, 'images')
 
         while True:
-            t_lap_start = time.time()
-
-            # save image
-            file_id = f'{i_iter:03}'
-            image_path = os.path.join(dir_image, f'{file_id}.png')
-            dcn.save_image(image_path, i_iter)
-
-            # save gdf
-            gdf_path = os.path.join(dir_output, 'geojson', f'{file_id}.json')
-            dcn.to_gdf().to_file(gdf_path, driver='GeoJSON')
+            DCN1985Runner.save_partial(i_iter, dcn, dir_image, dir_output)
 
             dcn.log_error()
             if dcn.is_reasonably_complete:
@@ -97,13 +108,11 @@ class DCN1985Runner:
                     shrink_factor=i_iter / dcn.max_iterations,
                 )
 
-            t_now = time.time()
-            dt_all = t_now - t_start
-            dt_iter = t_now - t_lap_start
-            log.debug(f'⏱️{i_iter=}, {dt_all=:.2f}s, {dt_iter=:.2f}s')
             i_iter += 1
             if i_iter >= dcn.max_iterations:
-                log.warning(f'MAX_ITERATIONS({dcn.max_iterations}) reached.')
+                log.warning(
+                    f'🛑 MAX_ITERATIONS({dcn.max_iterations}) reached.'
+                )
                 break
 
         AnimatedGIF(os.path.join(dir_output, 'animated.gif')).write(dir_image)
