6eb6dfabe2c3c972114c550feaf97b1a9b7c4ce2
nuuuwan <nuuuwan@gmail.com>
2024-07-11 17:05:33 +0530

    DCN1985RenderParams

--- src/cac/algos/dcn/impl/DCN1985RenderParams.py
+++ src/cac/algos/dcn/impl/DCN1985RenderParams.py
@@ -6,5 +6,6 @@ class DCN1985RenderParams:
     # common
     title: str = ''
     start_value_unit: str = "area (%)"
-    end_value_unit: str = "value"
     start_total_value: float = 100
+    end_value_unit: str = "value"
+    
