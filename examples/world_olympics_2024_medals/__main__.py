ed417e530f39c36d56b6da9bd7ad9d67f5a4eeb3
nuuuwan <nuuuwan@gmail.com>
2024-10-28 09:33:27 +0530

    world_olympics_2024_medals - lint

--- examples/world_olympics_2024_medals/__main__.py
+++ examples/world_olympics_2024_medals/__main__.py
@@ -6,89 +6,85 @@ def main():  # noqa
     from cac import DCN1985, DCN1985AlgoParams, DCN1985RenderParams
 
     COUNTRY_TO_GOLD_MEDALS = {
-        'Algeria': 2,
-        'Argentina': 1,
-        'Australia': 18,
-        'Austria': 2,
-        'Azerbaijan': 2,
-        'Bahrain': 2,
-        'Belgium': 3,
-        'Botswana': 1,
-        'Brazil': 3,
-        'Bulgaria': 3,
-        'Canada': 9,
-        'Chile': 1,
-        'China': 40,
-        'Chinese Taipei': 2,
-        'Croatia': 2,
-        'Cuba': 2,
-        'Czechia': 3,
-        'Denmark': 2,
-        'Dominica': 1,
-        'Dominican Republic': 1,
-        'Ecuador': 1,
-        'Egypt': 1,
-        'Ethiopia': 1,
-        'France': 16,
-        'Georgia': 3,
-        'Great Britain': 14,
-        'Greece': 1,
-        'Guatemala': 1,
-        'Hong Kong, China': 2,
-        'Hungary': 6,
-        'Indonesia': 2,
-        'Iran': 3,
-        'Ireland': 4,
-        'Israel': 1,
-        'Italy': 12,
-        'Jamaica': 1,
-        'Japan': 20,
-        'Kazakhstan': 1,
-        'Kenya': 4,
-        'Morocco': 1,
-        'Netherlands': 15,
-        'New Zealand': 10,
-        'Norway': 4,
-        'Pakistan': 1,
-        'Philippines': 2,
-        'Poland': 1,
-        'Portugal': 1,
-        'Romania': 3,
-        'Saint Lucia': 1,
-        'Serbia': 3,
-        'Slovenia': 2,
-        'South Africa': 1,
-        'South Korea': 13,
-        'Spain': 5,
-        'Sweden': 4,
-        'Switzerland': 1,
-        'Thailand': 1,
-        'Tunisia': 1,
-        'Uganda': 1,
-        'Ukraine': 3,
-        'United States': 40,
-        'Uzbekistan': 8,
+        "Algeria": 2,
+        "Argentina": 1,
+        "Australia": 18,
+        "Austria": 2,
+        "Azerbaijan": 2,
+        "Bahrain": 2,
+        "Belgium": 3,
+        "Botswana": 1,
+        "Brazil": 3,
+        "Bulgaria": 3,
+        "Canada": 9,
+        "Chile": 1,
+        "China": 40,
+        "Chinese Taipei": 2,
+        "Croatia": 2,
+        "Cuba": 2,
+        "Czechia": 3,
+        "Denmark": 2,
+        "Dominica": 1,
+        "Dominican Republic": 1,
+        "Ecuador": 1,
+        "Egypt": 1,
+        "Ethiopia": 1,
+        "France": 16,
+        "Georgia": 3,
+        "Great Britain": 14,
+        "Greece": 1,
+        "Guatemala": 1,
+        "Hong Kong, China": 2,
+        "Hungary": 6,
+        "Indonesia": 2,
+        "Iran": 3,
+        "Ireland": 4,
+        "Israel": 1,
+        "Italy": 12,
+        "Jamaica": 1,
+        "Japan": 20,
+        "Kazakhstan": 1,
+        "Kenya": 4,
+        "Morocco": 1,
+        "Netherlands": 15,
+        "New Zealand": 10,
+        "Norway": 4,
+        "Pakistan": 1,
+        "Philippines": 2,
+        "Poland": 1,
+        "Portugal": 1,
+        "Romania": 3,
+        "Saint Lucia": 1,
+        "Serbia": 3,
+        "Slovenia": 2,
+        "South Africa": 1,
+        "South Korea": 13,
+        "Spain": 5,
+        "Sweden": 4,
+        "Switzerland": 1,
+        "Thailand": 1,
+        "Tunisia": 1,
+        "Uganda": 1,
+        "Ukraine": 3,
+        "United States": 40,
+        "Uzbekistan": 8,
     }
 
     TRANSLATE_IDX = {
-        None: 'Bahrain',
-        'Taiwan': 'Chinese Taipei',
-        None: 'Dominica',
-        'Dominican Rep.': 'Dominican Republic',
-        'United Kingdom': 'Great Britain',
-        None: 'Hong Kong, China',
-        None: 'Saint Lucia',
-        'United States of America': 'United States',
+        "Taiwan": "Chinese Taipei",
+        "Dominican Rep.": "Dominican Republic",
+        "United Kingdom": "Great Britain",
+        "United States of America": "United States",
     }
 
     gdf_world = geopandas.read_file(
-        geopandas.datasets.get_path('naturalearth_lowres')
+        geopandas.datasets.get_path("naturalearth_lowres")
     )
-    gdf_world = gdf_world[gdf_world['continent'] != 'Antarctica']
+    gdf_world = gdf_world[gdf_world["continent"] != "Antarctica"]
 
     values = []
     used_countries = []
-    country_list = gdf_world['name'].tolist()
+    country_list = gdf_world["name"].tolist()
     country_list_with_no_medals_data = []
     for country in country_list:
         country_k = country
@@ -101,17 +97,17 @@ def main():  # noqa
         else:
             country_list_with_no_medals_data.append(country)
 
-    print('NO MEDALS DATA')
+    print("NO MEDALS DATA")
     for country in sorted(country_list_with_no_medals_data):
         print(f"    '{country}': None, ")
 
-    print('NO GEO DATA')
+    print("NO GEO DATA")
     for country in sorted(
         set(COUNTRY_TO_GOLD_MEDALS.keys()) - set(used_countries)
     ):
         print(f"    None: '{country}', ")
 
-    gdf_world = gdf_world[gdf_world['name'].isin(used_countries)]
+    gdf_world = gdf_world[gdf_world["name"].isin(used_countries)]
     print(used_countries)
     print(values)
 
