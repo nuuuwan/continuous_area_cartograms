ece91802d4cccd11b440a2cb95ad886ba1bc91af
nuuuwan <nuuuwan@gmail.com>
2024-10-28 09:26:14 +0530

    GridCAC -lint

--- src/cac/extended/GridCAC.py
+++ src/cac/extended/GridCAC.py
@@ -8,13 +8,13 @@ from utils import Hash
 from cac.algos import DCN1985
 from utils_future import AnimatedGIF, Log, PillowUser
 
-log = Log('GridCAC')
+log = Log("GridCAC")
 
 
 class GridCAC(PillowUser):
     FRAMES_PER_STAGE = 10
     DURATION_PER_STAGE = 10
-    IMAGE_VERSION = '20240714.1510'
+    IMAGE_VERSION = "20240714.1510"
     PILLOW_BASE_FONT_SIZE = (
         15 * (DCN1985.BASE_FONT_SIZE / 10) * (DCN1985.DPI / 150)
     )
@@ -26,15 +26,15 @@ class GridCAC(PillowUser):
         return len(self.dcn_list_list)
 
     def build_cell(self, job_id: str, i: int, j: int):
-        path_id = f'{job_id}.{i}.{j}'
+        path_id = f"{job_id}.{i}.{j}"
         dcn = self.dcn_list_list[i][j]
 
         dir_path_cell = os.path.join(tempfile.gettempdir(), path_id)
         shutil.rmtree(dir_path_cell, ignore_errors=True)
         os.makedirs(dir_path_cell)
 
-        dcn.render_params.super_title = ''
-        dcn.render_params.footer_text = ''
+        dcn.render_params.super_title = ""
+        dcn.render_params.footer_text = ""
         image_path_list_for_row_original = dcn.build(
             dir_path_cell, do_build_animated_gif=False
         )
@@ -46,15 +46,11 @@ class GridCAC(PillowUser):
                 * len(image_path_list_for_row_original)
                 / self.FRAMES_PER_STAGE
             )
-            image_path_list_for_cell.append(
-                image_path_list_for_row_original[m]
-            )
+            image_path_list_for_cell.append(image_path_list_for_row_original[m])
         return image_path_list_for_cell
 
-    def build_row_combined_frame(
-        self, image_path_list_for_i, k, super_title, footer_text
-    ):
-        h = Hash.md5(
+    def get_hash(self, image_path_list_for_i, k, super_title, footer_text):
+        return Hash.md5(
             str(
                 dict(
                     image_path_list_for_i=image_path_list_for_i,
@@ -70,8 +66,13 @@ class GridCAC(PillowUser):
                 )
             )
         )
+
+    def build_row_combined_frame(
+        self, image_path_list_for_i, k, super_title, footer_text
+    ):
+        h = self.get_hash(image_path_list_for_i, k, super_title, footer_text)
         combined_image_path = os.path.join(
-            tempfile.gettempdir(), f'cac.dnc.{h}.png'
+            tempfile.gettempdir(), f"cac.dnc.{h}.png"
         )
         if os.path.exists(combined_image_path):
             return combined_image_path
@@ -84,20 +85,20 @@ class GridCAC(PillowUser):
         draw.text(
             (total_width / 2, self.PILLOW_BASE_FONT_SIZE * 2.5),
             super_title,
-            fill='black',
+            fill="black",
             font=self.get_font(self.PILLOW_BASE_FONT_SIZE * 2),
-            anchor='ms',
+            anchor="ms",
         )
         draw.text(
             (total_width / 2, height - self.PILLOW_BASE_FONT_SIZE * 2),
             footer_text,
-            fill='black',
+            fill="black",
             font=self.get_font(self.PILLOW_BASE_FONT_SIZE),
-            anchor='ms',
+            anchor="ms",
         )
 
         combined_im.save(combined_image_path)
-        log.debug_temp(f'Wrote {combined_image_path}')
+        log.debug_temp(f"Wrote {combined_image_path}")
         return combined_image_path
 
     def build_row_combined_frames(
@@ -131,7 +132,7 @@ class GridCAC(PillowUser):
     def build_final_animated_gif(
         self, dir_path: str, combined_image_path_list: list[str]
     ):
-        animated_gif_path = os.path.join(dir_path, 'animated.gif')
+        animated_gif_path = os.path.join(dir_path, "animated.gif")
         AnimatedGIF(
             animated_gif_path,
             total_duration_s=self.DURATION_PER_STAGE * len(self),
@@ -147,6 +148,6 @@ class GridCAC(PillowUser):
             combined_image_path_list.extend(
                 combined_image_path_list_for_row[::-1]
             )
-            log.debug(f'✅ Row {i+1}/{n} complete ')
+            log.debug(f"✅ Row {i+1}/{n} complete ")
 
         self.build_final_animated_gif(dir_path, combined_image_path_list)
